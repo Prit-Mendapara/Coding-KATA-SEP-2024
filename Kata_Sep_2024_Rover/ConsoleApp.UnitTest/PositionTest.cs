eaa6fdb3e816e31c726ecf4a172593f4c035d06f
Maitri <maitri.patel@gantner.com>
2024-09-06 14:44:27 +0530

    Added initial position check test

--- Kata_Sep_2024_Rover/ConsoleApp.UnitTest/PositionTest.cs
+++ Kata_Sep_2024_Rover/ConsoleApp.UnitTest/PositionTest.cs
@@ -1,6 +1,18 @@
+using ConsoleApp1;
+
 namespace ConsoleApp.UnitTest;
 
+[TestFixture]
 public class PositionTest
 {
-    
+    [Test]
+    public void Validate_Initial_Position()
+    {
+        //Act
+        var position = new Position();
+
+        //Assert
+        Assert.That(position.X, Is.Zero);
+        Assert.That(position.Y, Is.Zero);
+    }
 }
